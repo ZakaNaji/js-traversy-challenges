4de6c751fc41048e17e602cd0891f993ac934b9a
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-05-25 18:50:21 +0100

    challenge3: create a calculator fun that does basid arethmitic ops

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,14 @@
-function calculator() {}
+function calculator(a, b, op) {
+  const operations = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => a / b,
+  };
+  if (!operations[op]) {
+    throw new Error("Invalid operation");
+  }
+  return operations[op](a, b);
+}
 
 module.exports = calculator;

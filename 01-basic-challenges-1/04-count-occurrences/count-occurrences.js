6b44e0ea6fc406db71ccbf710ef8b3bd485d2f8d
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-05-25 18:58:01 +0100

    challenge4: find char occurences in a string

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,11 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  let count = 0;
+  for (let c of str) {
+    if (c === char) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = countOccurrences;

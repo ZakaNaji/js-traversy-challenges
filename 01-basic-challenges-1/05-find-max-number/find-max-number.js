1998cae97f390c57537fc46492170255be9b17f1
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-05-25 19:04:24 +0100

    challenge5: find max num in array

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,14 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  if (arr.length === 0) {
+    throw new Error("Array is empty");
+  }
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
 
 module.exports = findMaxNumber;

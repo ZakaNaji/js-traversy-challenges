f4403557a9d861a7d2cdd7ccbee1f90fa5163997
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-05-25 19:27:47 +0100

    challenge 6-1: function that upperCase the 1st letter of everyword; using loop + str functions(slice + upper/lower case)

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,10 @@
-function titleCase() {}
+function titleCase(str) {
+  let words = str.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] =
+      words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
+  }
+  return words.join(" ");
+}
 
 module.exports = titleCase;

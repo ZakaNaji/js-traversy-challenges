0eb3b91c4309418fae9188a28bd39670060cfc08
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-05-25 19:33:44 +0100

    challenge 6-2: same title case challenge,but this time using replace with regex: /\b\w/g => \b to find seperated words (boundaries); \w first char of word; g to apply it to all occurences

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,10 +1,5 @@
 function titleCase(str) {
-  let words = str.split(" ");
-  for (let i = 0; i < words.length; i++) {
-    words[i] =
-      words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
-  }
-  return words.join(" ");
+  return str.replace(/\b\w/g, (match) => match.toUpperCase());
 }
 
 module.exports = titleCase;

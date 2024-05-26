57bfbb170eae6bb339fbd44f4326ab014fc50d31
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-05-26 13:36:36 +0100

    challenge 9 : to count voels in str just loop over all it's chars(lowercase them) and check if they contain a voel => count++; then return the count

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,13 @@
-function countVowels() {}
+function countVowels(str) {
+  let formatedStr = str.toLowerCase();
+  let vowels = "aeiou";
+  let count = 0;
+  for (let i = 0; i < formatedStr.length; i++) {
+    if (vowels.includes(formatedStr[i])) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = countVowels;

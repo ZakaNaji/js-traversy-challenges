bd0e0bda2b7e11d1c95d62afe1f963ee1e83898f
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-05-26 13:31:21 +0100

    challenge 8: Palendrome -> travers str from start (i=0) and end (length - i -1) simultanusly; if all of them match we have a palendrome else no | alternatively we can just compare the str with it's reverse (after we lowerCase it and remove all non alphabitics

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,12 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  let cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const length = cleanedStr.length;
+  for (let i = 0; i < length; i++) {
+    if (cleanedStr[i] !== cleanedStr[length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 module.exports = isPalindrome;

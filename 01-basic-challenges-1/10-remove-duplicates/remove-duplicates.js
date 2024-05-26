d04c6a3675213d52003494cdc7f2ccac6b86a698
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-05-26 13:44:29 +0100

    challenge 10 - remove duplicates from an array: - create uniqueArr = [] - loop over arr elements and check if uniqueArr have is (uniqueArr.include[arr[i]] or indexOf(arr[i]) === -1) then add it to uniqueArr - alternatevely you can use Set retrun Array.from(new Set(arr))

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,11 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (newArr.indexOf(arr[i]) === -1) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
 
 module.exports = removeDuplicates;

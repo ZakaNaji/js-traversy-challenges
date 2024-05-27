30f84d992dd22d91618312ebd72691a3ce3cc01f
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-05-27 22:45:15 +0100

    challenge 2-1: FuzzBuzz: - loop ints untill number - check i%3 === 0 add Fizz - check i%5 === à add Buzz - if bouth add FizzBuzz - if nothing of the above add i

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,21 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+  let fizzBuzz = "";
+  let fizzBuzzArray = [];
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0) {
+      fizzBuzz += "Fizz";
+    }
+    if (i % 5 === 0) {
+      fizzBuzz += "Buzz";
+    }
+    if (fizzBuzz) {
+      fizzBuzzArray.push(fizzBuzz);
+      fizzBuzz = "";
+    } else {
+      fizzBuzzArray.push(i);
+    }
+  }
+  return fizzBuzzArray;
+}
 
 module.exports = fizzBuzzArray;

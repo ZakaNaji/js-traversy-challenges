ed48a3762ae4527bd3887279db355cfb82730b6d
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-05-30 20:53:31 +0100

    challenge 2-4: find missing unique number: - use formula expectedNumner - actualNumber - expected number = n*(n+1)/2 - actual number (use reduce/or for) = arr.reduce(sum, num => sum + num,0)

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,11 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  if (!arr || arr.length === 0) {
+    return undefined;
+  }
+  let n = arr.length + 1;
+  let expectedSum = (n * (n + 1)) / 2;
+  let actualSum = arr.reduce((sum, num) => sum + num, 0);
+  return expectedSum - actualSum;
+}
 
 module.exports = findMissingNumber;

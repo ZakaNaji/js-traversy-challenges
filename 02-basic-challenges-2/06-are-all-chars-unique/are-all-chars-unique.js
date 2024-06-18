0f913109843cc5da4c58222e6316b1017b44129f
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-06-18 23:17:55 +0100

    challenge 2-6: check all chars of str are unique; just loop all the chars of string and compare with a set if chars exist false else if you get out of the loop true;

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,13 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  let chars = {};
+  for (let i = 0; i < str.length; i++) {
+    if (chars[str[i]]) {
+      return false;
+    } else {
+      chars[str[i]] = str[i];
+    }
+  }
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
